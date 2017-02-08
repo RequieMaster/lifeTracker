9a98953793b00419ef35c478290a123be741e8e9
RequieMaster <sindi.corral@gmail.com>
2017-02-08 11:38:07 +0100

    Cambios

--- public/javascripts/goalController.js
+++ public/javascripts/goalController.js
@@ -1,21 +1,21 @@
 angular.module('goalController', [])
 .controller('goalCtrl', function($scope, $http, Goals) {
   Goals.get().success(function(data) {
-    for (var currentIndex=0; currentIndex<data.length; currentIndex=currentIndex+1) {
-      currentelement = data[currentIndex];
-      currentelement.ok= currentIndex;
-      currentelement.ko= 1;
-      currentelement.percentage = (currentelement.ok/(currentelement.ok+currentelement.ko))*100;
+    for (var i=0;i<data.length; i++) {
+      data[i].ok = i;
+      data[i].ko = 1;
+      data[i].percentage = (data[i].ok/(data[i].ok+data[i].ko))*100;
     }
-      $scope.goals = data;
+    $scope.goals = data;
   });
   $scope.addGoal = function() {
     if(!$scope.goalName || $scope.goalName === '') { return; }
     var goalObject = {
       description: $scope.goalName
-    };
+     }
     Goals.create(goalObject).success(function(data) {
       goalObject._id=data._id;
+      goalObject.percentage = 0;
       $scope.goals.push(goalObject);
       $scope.goalName = '';
     });
